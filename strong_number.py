03c5c5cfc9a8398aa6a0237285f658f423922593
mj-weshh <jowamumjwesh@gmail.com>
2024-03-27 22:49:50 +0300

    first commit

--- strong_number.py
+++ strong_number.py
@@ -10,30 +10,39 @@ Examples
 145 is a strong number, because 1! + 4! + 5! = 1 + 24 + 120 = 145, so return "STRONG!!!!".
 150 is not a strong number, because 1! + 5! + 0! = 122 is not equal to 150, so return "Not Strong !!".
 """
-#factorail function takes in a string input, conver
+
 def factorial(num):
-     factorail_product = 1
+     #converts the str argument to an int
      num = int(num)
+     #initializes the factorial product to 1
+     factorail_product = 1
      
+     #calculates the factorial product
      while num >= 1:
           factorail_product *= num
           num -=1
      
      return factorail_product
 
+
 def strong(number):
+     #number is a str
      number_int = int(number)
      factorials= []
      sum = 0
      status = ''
      
      for i in number:
+          #appends the digit elements into factorials
           factorials.append(i)
      
      for j in factorials:
+          #calls factorial() and sums up all the factorials
           sum += int(factorial(j))
+     #outputs sum of factorials
      print(f"Sum of factorials is: {sum}")
-     
+
+     #checks whether the number is strong or not
      if sum == number_int:
           status = "Strong!!!"
      else:
@@ -43,10 +52,15 @@ def strong(number):
 
 
 def main():
+     #user inputs a number (recieved as a str)
      number = input("Enter number: ")
+     #calls strong() 
      status =  strong(number)
+     #outputs the status of the number
+     #whether strong or not strong
      print(f"{number} is {status}")
 
 
 if __name__ == "__main__":
+     #calls main()
      main()
